b2aad35b1ad1d9df5ee84e1ad6d5d0ff9d47a372
jonmayoh <jon.mayoh@gmail.com>
2022-12-10 11:38:10 +0000

    sandbox.js

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+  container: 'bm', // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'samurai.json' // the path to the animation json
+});
